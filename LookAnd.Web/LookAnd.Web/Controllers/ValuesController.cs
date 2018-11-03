a92f1bbb5ed8db4e24bd73fe9d1d4d0439714384
JohnGaltt <johngaltaregoing@gmail.com>
2018-11-03 15:05:53 +0200

    Itegration with img post

--- LookAnd.Web/LookAnd.Web/Controllers/ValuesController.cs
+++ LookAnd.Web/LookAnd.Web/Controllers/ValuesController.cs
@@ -24,11 +24,68 @@ namespace LookAnd.Web.Controllers
       return "value";
     }
 
+
+    // POST api/values
+    [HttpPost]
+    public async Task PostImage([FromBody] string value)
+    {
+      string imagePath = @"https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/befbcde0-9b36-11e6-95b9-00163ed833e7/260663710/the-test-fun-for-friends-screenshot.jpg";
+
+      string visionApiKey = "8a9c440964b64b07bc84a1b2af6b6384";
+      string visionApiEndPoint = "https://westeurope.api.cognitive.microsoft.com";
+
+      HttpClient client = new HttpClient();
+
+      client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", visionApiKey);
+
+      // Request parameters. A third optional parameter is "details".
+      string requestParameters = "visualFeatures=Categories,Description,Color&language=en";
+
+      // Assemble the URI for the REST API Call.
+      string uri = visionApiEndPoint + "/analyze" + "?" + requestParameters;
+
+      HttpResponseMessage response;
+
+      // Request body. Posts an image you've added to your site's images folder.
+      var fileInfo = env.WebRootFileProvider.GetFileInfo(imagePath);
+      byte[] byteData = ProgramHelper.GetImageAsByteArray(fileInfo.PhysicalPath);
+
+      string contentString = string.Empty;
+      using (ByteArrayContent content = new ByteArrayContent(byteData))
+      {
+        // This example uses content type "application/octet-stream".
+        // The other content types you can use are "application/json" and "multipart/form-data".
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+        // Execute the REST API call.
+        response = client.PostAsync(uri, content).Result;
+
+        // Get the JSON response.
+        contentString = response.Content.ReadAsStringAsync().Result;
+      }
+    }
+
+//    if (env.IsDevelopment())
+//    {
+//      app.UseDeveloperExceptionPage();
+//    }
+//
+//    app.Run(async (context) =>
+//    {
+//      await context.Response.WriteAsync("<h1>Cognitive Services Demo</h1>");
+//      await context.Response.WriteAsync($"<p><b>Test Image:</b></p>");
+//      await context.Response.WriteAsync($"<div><img src=\"" + imagePath + "\" /></div>");
+//      await context.Response.WriteAsync($"<p><b>Computer Vision API results:</b></p>");
+//      await context.Response.WriteAsync("<p>");
+//      await context.Response.WriteAsync(ProgramHelper.JsonPrettyPrint(contentString));
+//      await context.Response.WriteAsync("<p>");
+//    });
+
     // POST api/values
     [HttpPost]
     public void Post([FromBody] string value)
     {
-      
+
     }
 
     // PUT api/values/5
---
+++ LookAnd.Web/LookAnd.Web/Helper/Helper.cs
@@ -0,0 +1,83 @@
+namespace LookAnd.Web.Helper
+{
+   public static class ProgramHelper
+    {
+        /// <summary>
+     /// Returns the contents of the specified file as a byte array.
+     /// </summary>
+     /// <param name="imageFilePath">The image file to read.</param>
+     /// <returns>The byte array of the image data.</returns>
+     public static byte[] GetImageAsByteArray(string imageFilePath)
+     {
+         FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+         BinaryReader binaryReader = new BinaryReader(fileStream);
+         return binaryReader.ReadBytes((int)fileStream.Length);
+     }
+
+     /// <summary>
+     /// Formats the given JSON string by adding line breaks and indents.
+     /// </summary>
+     /// <param name="json">The raw JSON string to format.</param>
+     /// <returns>The formatted JSON string.</returns>
+     public static string JsonPrettyPrint(string json)
+     {
+         if (string.IsNullOrEmpty(json))
+             return string.Empty;
+
+         json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+         string INDENT_STRING = "    ";
+         var indent = 0;
+         var quoted = false;
+         var sb = new StringBuilder();
+         for (var i = 0; i < json.Length; i++)
+         {
+             var ch = json[i];
+             switch (ch)
+             {
+                 case '{':
+                 case '[':
+                     sb.Append(ch);
+                     if (!quoted)
+                     {
+                         sb.AppendLine();
+                     }
+                     break;
+                 case '}':
+                 case ']':
+                     if (!quoted)
+                     {
+                         sb.AppendLine();
+                     }
+                     sb.Append(ch);
+                     break;
+                 case '"':
+                     sb.Append(ch);
+                     bool escaped = false;
+                     var index = i;
+                     while (index > 0 && json[--index] == '\\')
+                         escaped = !escaped;
+                     if (!escaped)
+                         quoted = !quoted;
+                     break;
+                 case ',':
+                     sb.Append(ch);
+                     if (!quoted)
+                     {
+                         sb.AppendLine();
+                     }
+                     break;
+                 case ':':
+                     sb.Append(ch);
+                     if (!quoted)
+                         sb.Append(" ");
+                     break;
+                 default:
+                     sb.Append(ch);
+                     break;
+             }
+         }
+         return sb.ToString();
+     }
+    }
+}
