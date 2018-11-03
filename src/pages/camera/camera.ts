169ef681d78e42ee4cd0f55ce6d4d02d439061e0
Alexander Demianenko <ale.de635@gmail.com>
2018-11-03 11:02:34 +0200

    web basis and camera added for the fuck sake

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,28 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { NavController, AlertController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'camera-ionic',
   templateUrl: 'camera.html'
 })
-export class CameraPage {
-  options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
+export class CameraPage implements OnInit, OnDestroy{
+
+  private image: string;
+
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera,
+    public alertCtrl: AlertController,
+    private domSanitizer: DomSanitizer) {
   }
 
-  constructor(private camera: Camera) {
+  ngOnInit(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      saveToPhotoAlbum: true,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        this.displayErrorAlert(err);
+      });
   }
 
-  takePicture(){
-    this.camera.getPicture(this.options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-    });
+  ngOnDestroy(){
+    this.image = '';
+  }
+
+  displayErrorAlert(err){
+    console.log(err);
+    let alert = this.alertCtrl.create({
+       title: 'Error',
+       subTitle: 'Error while trying to capture picture',
+       buttons: ['OK']
+     });
+     alert.present();
   }
 }
